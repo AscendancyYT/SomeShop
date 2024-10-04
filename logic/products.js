e8dc4615cf4fd8a3468fe124a8561993cbe2bccb
Single-Phantom <misterminecraftter@gmail.com>
2024-10-04 13:07:42 +0500

    cart

--- logic/products.js
+++
@@ -1,9 +0,0 @@
-// let addToCart = document.querySelector(".addToCart");
-
-document.querySelector(".addToCart").addEventListener("click", function (e) {
-  e.preventDefault();
-
-  axios.get("http://localhost:5000/Products").then((response)=>{
-    let productInfo = response.data
-  });
-});
